f59d2fe21acdfd158146775e7c44b3e949ec686e
Sznapsollo <office@webproject.waw.pl>
2026-03-05 09:30:53 +0100

    change default 100Kb payload to 50 mb

--- models/server.js
+++ models/server.js
@@ -18,7 +18,8 @@ class Server {
 
   middlewares() {
     this.app.use(cors());
-    this.app.use(express.json());
+    this.app.use(express.json({ limit: "50mb" }));
+    this.app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
     this.app.use(
       express.static(path.join(__dirname, "../client/build"))
